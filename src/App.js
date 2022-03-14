a19efbda66d994a94e0ed441ae4da70eec29fabc
pankajan <pankajan05@gmail.com>
2022-03-14 07:04:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-/* eslint-disable */
-// /**
-// =========================================================
-// * Material Dashboard 2 React - v2.0.0
-// =========================================================
-
-// * Product Page: https://www.creative-tim.com/product/material-dashboard-react
-// * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-// Coded by www.creative-tim.com
-
-//  =========================================================
-
-// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// */
-
 import { useState, useEffect, useMemo } from "react";
 
 // react-router components
@@ -33,15 +17,12 @@ import Configurator from "examples/Configurator";
 
 // Material Dashboard 2 React themes
 import theme from "assets/theme";
-import themeRTL from "assets/theme/theme-rtl";
 
 // Material Dashboard 2 React Dark Mode themes
 import themeDark from "assets/theme-dark";
-import themeDarkRTL from "assets/theme-dark/theme-rtl";
 
 // RTL plugins
 import rtlPlugin from "stylis-plugin-rtl";
-import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 
 // Material Dashboard 2 React routes
@@ -57,7 +38,7 @@ import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "co
 // Images
 import brandWhite from "assets/images/logo-ct.png";
 import brandDark from "assets/images/logo-ct-dark.png";
-import { AuthProvider, useAuth } from "./auth-context";
+import { useAuth } from "./auth-context";
 
 export default function App() {
   // const [navbar, setNavbar] = useState(false);
@@ -74,6 +55,7 @@ export default function App() {
     darkMode,
   } = controller;
   const [onMouseEnter, setOnMouseEnter] = useState(false);
+  // eslint-disable-next-line no-unused-vars
   const [rtlCache, setRtlCache] = useState(null);
   const { pathname } = useLocation();
 
@@ -154,8 +136,6 @@ export default function App() {
     </MDBox>
   );
 
-  console.log(loggedIn); 
-  
   return loggedIn ? (
     <ThemeProvider theme={darkMode ? themeDark : theme}>
       <CssBaseline />
@@ -192,206 +172,10 @@ export default function App() {
         </>
       )}
       {layout === "vr" && <Configurator />}
-        <Routes>
-          {getRoutes(routes)}
-          <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
-        </Routes>
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+      </Routes>
     </ThemeProvider>
   );
-    
 }
-
-// import { useState, useEffect, useMemo } from "react";
-
-// // react-router components
-// import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
-// // @mui material components
-// import { ThemeProvider } from "@mui/material/styles";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import Icon from "@mui/material/Icon";
-
-// // Material Dashboard 2 React components
-// import MDBox from "components/MDBox";
-
-// // Material Dashboard 2 React example components
-// import Sidenav from "examples/Sidenav";
-// import Configurator from "examples/Configurator";
-
-// // Material Dashboard 2 React themes
-// import theme from "assets/theme";
-// import themeRTL from "assets/theme/theme-rtl";
-
-// // Material Dashboard 2 React Dark Mode themes
-// import themeDark from "assets/theme-dark";
-// import themeDarkRTL from "assets/theme-dark/theme-rtl";
-
-// // RTL plugins
-// import rtlPlugin from "stylis-plugin-rtl";
-// import { CacheProvider } from "@emotion/react";
-// import createCache from "@emotion/cache";
-
-// // Material Dashboard 2 React routes
-// import routes from "routes";
-
-// // Material Dashboard 2 React contexts
-// import { useMaterialUIController, setMiniSidenav, setOpenConfigurator } from "context";
-
-// // Images
-// import brandWhite from "assets/images/logo-ct.png";
-// import brandDark from "assets/images/logo-ct-dark.png";
-
-// export default function App() {
-//   const [controller, dispatch] = useMaterialUIController();
-//   const {
-//     miniSidenav,
-//     direction,
-//     layout,
-//     openConfigurator,
-//     sidenavColor,
-//     transparentSidenav,
-//     whiteSidenav,
-//     darkMode,
-//   } = controller;
-//   const [onMouseEnter, setOnMouseEnter] = useState(false);
-//   const [rtlCache, setRtlCache] = useState(null);
-//   const { pathname } = useLocation();
-
-//   // Cache for the rtl
-//   useMemo(() => {
-//     const cacheRtl = createCache({
-//       key: "rtl",
-//       stylisPlugins: [rtlPlugin],
-//     });
-
-//     setRtlCache(cacheRtl);
-//   }, []);
-
-//   // Open sidenav when mouse enter on mini sidenav
-//   const handleOnMouseEnter = () => {
-//     if (miniSidenav && !onMouseEnter) {
-//       setMiniSidenav(dispatch, false);
-//       setOnMouseEnter(true);
-//     }
-//   };
-
-//   // Close sidenav when mouse leave mini sidenav
-//   const handleOnMouseLeave = () => {
-//     if (onMouseEnter) {
-//       setMiniSidenav(dispatch, true);
-//       setOnMouseEnter(false);
-//     }
-//   };
-
-//   // Change the openConfigurator state
-//   const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-
-//   // Setting the dir attribute for the body element
-//   useEffect(() => {
-//     document.body.setAttribute("dir", direction);
-//   }, [direction]);
-
-//   // Setting page scroll to 0 when changing the route
-//   useEffect(() => {
-//     document.documentElement.scrollTop = 0;
-//     document.scrollingElement.scrollTop = 0;
-//   }, [pathname]);
-
-//   const getRoutes = (allRoutes) =>
-//     allRoutes.map((route) => {
-//       if (route.collapse) {
-//         return getRoutes(route.collapse);
-//       }
-
-//       if (route.route) {
-//         return <Route exact path={route.route} element={route.component} key={route.key} />;
-//       }
-
-//       return null;
-//     });
-
-//   const configsButton = (
-//     <MDBox
-//       display="flex"
-//       justifyContent="center"
-//       alignItems="center"
-//       width="3.25rem"
-//       height="3.25rem"
-//       bgColor="white"
-//       shadow="sm"
-//       borderRadius="50%"
-//       position="fixed"
-//       right="2rem"
-//       bottom="2rem"
-//       zIndex={99}
-//       color="dark"
-//       sx={{ cursor: "pointer" }}
-//       onClick={handleConfiguratorOpen}
-//     >
-//       <Icon fontSize="small" color="inherit">
-//         settings
-//       </Icon>
-//     </MDBox>
-//   );
-
-//   return direction === "rtl" ? (
-//     <CacheProvider value={rtlCache}>
-//       <ThemeProvider theme={darkMode ? themeDarkRTL : themeRTL}>
-//         <CssBaseline />
-//         {layout === "dashboard" && (
-//           <>
-//             <Sidenav
-//               color={sidenavColor}
-//               brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-//               brandName="Material Dashboard 2"
-//               routes={routes}
-//               onMouseEnter={handleOnMouseEnter}
-//               onMouseLeave={handleOnMouseLeave}
-//             />
-//             <Configurator />
-//             {configsButton}
-//           </>
-//         )}
-//         {layout === "vr" && <Configurator />}
-//         <Routes>
-//           {getRoutes(routes)}
-//           <Route path="*" element={<Navigate to="/dashboard" />} />
-//         </Routes>
-//       </ThemeProvider>
-//     </CacheProvider>
-//   ) : (
-//     <ThemeProvider theme={darkMode ? themeDark : theme}>
-//       <CssBaseline />
-//       {layout === "dashboard" && (
-//         <>
-//           <Sidenav
-//             color={sidenavColor}
-//             brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-//             brandName="Material Dashboard 2"
-//             routes={routes}
-//             onMouseEnter={handleOnMouseEnter}
-//             onMouseLeave={handleOnMouseLeave}
-//           />
-//           <Configurator />
-//           {configsButton}
-//         </>
-//       )}
-//       {layout === "vr" && <Configurator />}
-//       <Routes>
-//         {getRoutes(routes)}
-//         <Route path="*" element={<Navigate to="/dashboard" />} />
-//       </Routes>
-//     </ThemeProvider>
-//   );
-// }
-
-/**
-=========================================================
-* Material Dashboard 2 React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
