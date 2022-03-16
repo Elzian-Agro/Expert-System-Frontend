b56163f1c32859a13f1d9c0a7a2f9cc528636eb7
RimazRizkan <rimaz660066@gmail.com>
2022-03-16 11:17:34 +0530

    Update ExpertCard.js

--- src/components/Experts/ExpertCard.js
+++ src/components/Experts/ExpertCard.js
@@ -21,49 +21,53 @@ const Img = styled("img")({
 const ExpertCard = (props) => {
   const { name, description, position, organization, expImage } = props;
   return (
-    <Card sx={{ width: 600, mb: 5, margin: "auto", height: "auto", my: 4 }}>
-      <CardContent sx={{ paddingTop: 3 }}>
-        <Grid container justifyContent="center" alignItems="center" spacing={2}>
-          <Grid item paddingRight={2} justifyContent="center" alignItems="center">
-            <Img
-              alt="Expert Profile"
-              // setting a default image if profile image is null
-              src={
-                expImage != null
-                  ? `https://elzian-agro-user-auth.herokuapp.com/uploads/images/${expImage}`
-                  : "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=160"
-              }
-            />
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <MDTypography gutterBottom variant="h5" component="div" align="justify">
-                  {name}
-                </MDTypography>
-                <MDTypography variant="body2" gutterBottom>
-                  {description}
-                </MDTypography>
+    <Grid container alignItems="center" justifyContent="center">
+      <Grid item style={{ width: "550px" }}>
+        <Card sx={{ maxWidth: 600, mb: 5, margin: "auto", height: "auto", my: 4 }}>
+          <CardContent sx={{ paddingTop: 3 }}>
+            <Grid container justifyContent="center" alignItems="center" spacing={2}>
+              <Grid item paddingRight={2} justifyContent="center" alignItems="center">
+                <Img
+                  alt="Expert Profile"
+                  // setting a default image if profile image is null
+                  src={
+                    expImage != null
+                      ? `https://elzian-agro-user-auth.herokuapp.com/uploads/images/${expImage}`
+                      : "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=160"
+                  }
+                />
               </Grid>
-              <Grid item xs container spacing={2} justifyContent="space-between">
-                <Grid item xs={4}>
-                  <MDTypography variant="body2" gutterBottom>
-                    <AccountCircleRoundedIcon sx={{ mx: 1 }} />
-                    {position}
-                  </MDTypography>
-                </Grid>
-                <Grid item xs={8}>
-                  <MDTypography variant="body2" gutterBottom>
-                    <CorporateFareRoundedIcon sx={{ mx: 1 }} />
-                    {organization}
-                  </MDTypography>
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs>
+                    <MDTypography gutterBottom variant="h5" component="div" align="justify">
+                      {name}
+                    </MDTypography>
+                    <MDTypography variant="body2" gutterBottom>
+                      {description}
+                    </MDTypography>
+                  </Grid>
+                  <Grid item xs container spacing={2} justifyContent="space-between">
+                    <Grid item xs={4}>
+                      <MDTypography variant="body2" gutterBottom>
+                        <AccountCircleRoundedIcon sx={{ mx: 1 }} />
+                        {position}
+                      </MDTypography>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <MDTypography variant="body2" gutterBottom>
+                        <CorporateFareRoundedIcon sx={{ mx: 1 }} />
+                        {organization}
+                      </MDTypography>
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
