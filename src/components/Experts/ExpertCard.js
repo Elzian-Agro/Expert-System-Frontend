e12673ffad83ed5022e2c1c4e30bc71823f1c53b
RimazRizkan <rimaz660066@gmail.com>
2022-03-16 10:48:03 +0530

    Adjusted expert card size accordingly

--- src/components/Experts/ExpertCard.js
+++ src/components/Experts/ExpertCard.js
@@ -3,8 +3,8 @@ import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Grid from "@mui/material/Grid";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import CorporateFareRoundedIcon from "@mui/icons-material/CorporateFareRounded";
 import MDTypography from "components/MDTypography";
 
 import PropTypes from "prop-types";
@@ -21,10 +21,10 @@ const Img = styled("img")({
 const ExpertCard = (props) => {
   const { name, description, position, organization, expImage } = props;
   return (
-    <Card sx={{ maxWidth: 850, mb: 5, margin: "auto", width: "auto", height: "auto", my: 4 }}>
-      <CardContent>
+    <Card sx={{ width: 600, mb: 5, margin: "auto", height: "auto", my: 4 }}>
+      <CardContent sx={{ paddingTop: 3 }}>
         <Grid container justifyContent="center" alignItems="center" spacing={2}>
-          <Grid item paddingRight={2}>
+          <Grid item paddingRight={2} justifyContent="center" alignItems="center">
             <Img
               alt="Expert Profile"
               // setting a default image if profile image is null
@@ -48,13 +48,13 @@ const ExpertCard = (props) => {
               <Grid item xs container spacing={2} justifyContent="space-between">
                 <Grid item xs={4}>
                   <MDTypography variant="body2" gutterBottom>
-                    <PersonOutlineIcon sx={{ mx: 1 }} />
+                    <AccountCircleRoundedIcon sx={{ mx: 1 }} />
                     {position}
                   </MDTypography>
                 </Grid>
                 <Grid item xs={8}>
                   <MDTypography variant="body2" gutterBottom>
-                    <CorporateFareIcon sx={{ mx: 1 }} />
+                    <CorporateFareRoundedIcon sx={{ mx: 1 }} />
                     {organization}
                   </MDTypography>
                 </Grid>
