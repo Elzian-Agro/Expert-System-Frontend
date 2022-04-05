1ec36312ac741202840ccfc2ed7ac112dfaccdcf
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2022-04-05 19:49:50 +0530

    My Schedule , Overall Schedule and Create Schedule page documentations added.

---
+++ src/layouts/documentation/docs/mySchedule.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+import Screen1 from "../../../assets/images/My schedule 1.png";
+import Screen2 from "../../../assets/images/My schedule 2.png";
+
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  width: "100%",
+});
+
+function MySchedule() {
+  return (
+    <>
+      <Box>
+        <Typography variant="h1" component="div" gutterBottom>
+          My Schedule
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          1.Navigate to My Schedule page
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          If you are log in you will automatically navigate to Dashboard. Then you can see the side
+          navigation bar at the left corner of the page. Then you can see many options such as My
+          schedule, Create schedule, Overall schedule, Experts, Contact-Us, Profile and About us.
+          You can click My Schedule button and you will navigate to My Schedule page. And also you
+          can see booked schedules in Dashboard named as meeting links.
+        </Typography>
+        <br />
+        <Img
+          alt="screenshot of My Schedule user documentation"
+          // setting a default image if profile image is null
+          src={Screen1}
+        />
+        <br />
+        <br />
+        <Typography variant="h3" gutterBottom>
+          2.Use My Schedule Page
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          If you navigate to My Schedule page you can see booked schedules in your page. You can use
+          the meeting link to join the meeting on allocated time. And also you can find the meeting
+          links in dashboard. If you don’t have any booked schedule you will get alert message.
+        </Typography>
+        <br />
+        <Img
+          alt="screenshot of My Schedule user documentation"
+          // setting a default image if profile image is null
+          src={Screen2}
+        />
+      </Box>
+    </>
+  );
+}
+
+export default MySchedule;
---
+++ src/layouts/documentation/docs/overallSchedule.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+import Screen1 from "../../../assets/images/Overall schedule1.png";
+import Screen2 from "../../../assets/images/Overall schedule 2.png";
+
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  width: "100%",
+});
+function OverallSchedule() {
+  return (
+    <>
+      <Box>
+        <Typography variant="h1" component="div" gutterBottom>
+          Overall Schedule
+        </Typography>
+        <Typography variant="h3" gutterBottom>
+          1.Navigate to Overall Schedule Page
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          If you are log in you will automatically navigate to Dashboard. Then you can see the side
+          navigation bar at the left corner of the page. Then you can see many options such as My
+          schedule, Create schedule, Overall schedule, Experts, Contact-Us, Profile and About us.
+          You can click Overall Schedule button and you will navigate to Overall Schedule page.
+        </Typography>
+        <br />
+
+        <Img
+          alt="screenshot of My Schedule user documentation"
+          // setting a default image if profile image is null
+          src={Screen1}
+        />
+        <br />
+        <br />
+
+        <Typography variant="h3" gutterBottom>
+          2.Use Overall Schedule Page
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          You can see many schedule cards here. If you book the schedule it will appear in My
+          Schedule page and that will disappear in Overall Schedule.
+        </Typography>
+        <Img
+          alt="screenshot of My Schedule user documentation"
+          // setting a default image if profile image is null
+          src={Screen2}
+        />
+      </Box>
+    </>
+  );
+}
+
+export default OverallSchedule;
